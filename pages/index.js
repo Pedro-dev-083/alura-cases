77b8f5b2947014dae34000f9045ee612f21be29d
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2023-12-14 17:32:03 -0300

    Aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import Link from 'next/link';
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage(){
-    return(
-        <div>
-            <h1>Alura Cases - Home Page</h1>
-            <Link href="/faq" passHref>Ir para o FAQ</Link>
-        </div>
-    )
-}
+export default HomeScreen;
+
+// import Link from 'next/link';
+// import Footer from '../src/components/patterns/footer';
+
+
+// function Title({children, as}){
+//     const Tag = as;
+//     return(
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag}{
+//                     color: red;
+//                     font-family: sans-serif;
+//                 }
+//             `}</style>
+//         </>
+//     );
+// }
+
+// export default function HomePage(){
+//     return(
+//         <div>
+//             <Title as="h1">Alura Cases - Home Page</Title>
+//             <Link href="/faq" passHref>Ir para o FAQ</Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
